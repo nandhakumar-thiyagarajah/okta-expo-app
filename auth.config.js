7642f802a268b43289185f80bcbeb4948a365812
nthiyagarjah <nthiyagarjah@dsservices.com>
2024-03-04 21:01:24 +0530

    okta changes

---
+++ auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  oidc:{
+    clientId: '0oafi6v6cy6gGQwPS5d7',
+    redirectUri: 'com.okta.dev-53163714:/callback',
+    endSessionRedirectUri: 'com.okta.dev-53163714:/logout',
+    discoveryUri: 'https://dev-53163714.okta.com/oauth2/default',
+    scopes: ['openid', 'profile','email'],
+    requireHardwareBackedKeyStore: false,
+   }
+  };
